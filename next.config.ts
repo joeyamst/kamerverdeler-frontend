b50c3278d4c1ed9ca36bb11a82ad2ec5728e08ce
Joey <joey.hendriks@amsterdam.nl>
2025-08-07 16:21:14 +0200

    added pages config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   typescript: {
       ignoreBuildErrors: true
-  }
+  },
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable default image optimization
+    },
+    assetPrefix: isProd ? '/kamerverdeler-frontend/' : '',
+    basePath: isProd ? '/kamerverdeler-frontend' : '',
+    output: 'export'
 };
 
 export default nextConfig;
