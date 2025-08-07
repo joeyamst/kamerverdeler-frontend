9e90dabd30908b6e41e06bbea868d02c252ff914
Joey <joey.hendriks@amsterdam.nl>
2025-08-07 14:16:11 +0200

    init commit

---
+++ app/kamerverdeler.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import "@amsterdam/design-system-assets/font/index.css"
+import "@amsterdam/design-system-css/dist/index.css"
+import "@amsterdam/design-system-tokens/dist/index.css"
+import {Heading, Paragraph} from "@amsterdam/design-system-react"
+
+function Kamerverdeler() {
+    return <div>
+        <Heading level={1}>
+            Kamerverdeler applicatie
+        </Heading>
+        <Paragraph>
+            Hier komt een kamerverdeler
+        </Paragraph>
+    </div>
+}
+
+export default Kamerverdeler
